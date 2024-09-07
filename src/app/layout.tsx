431a840edfa06481979f7e652dcc093fcb293caf
Emmanuel Xs <emmanuelxs101@gmail.com>
2024-09-07 14:28:04 +0100

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { insSans } from "@/lib/font";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | devlinks",
+    default: "devlinks",
+  },
+  description: "All social links about you in one app",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${insSans.variable}  antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ src/lib/font.ts
@@ -0,0 +1,8 @@
+import { Instrument_Sans } from "next/font/google";
+
+export const insSans = Instrument_Sans({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  variable: "--font-ins-sans",
+  display: "swap",
+});
