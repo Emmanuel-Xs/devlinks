6b63395eba54b32997a40bf4487e20a484b0d3bf
Emmanuel Xs <emmanuelxs101@gmail.com>
2024-09-11 17:54:38 +0100

    feat: remove the devlinks text on mobile

--- src/components/ui/devlinks-logo.tsx
+++ src/components/ui/devlinks-logo.tsx
@@ -1,24 +1,31 @@
+"use client";
 import { cn } from "@/lib/utils";
+import { useMediaQuery } from "@uidotdev/usehooks";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export default function DevlinksLogo({ className }: { className?: string }) {
+  const isSmUp = useMediaQuery("only screen and (min-width : 500px)");
   return (
-    <div className={cn("flex w-fit items-center gap-1", className)}>
+    <Link className={cn("flex w-fit items-center gap-1", className)} href="/">
       <Image
         src="/logos/devlinksLogo.svg"
         alt="icon logo of devlinks"
         priority
         width="32"
         height="32"
+        title={isSmUp ? "" : "devlinks logo"}
       />
-      <Image
-        src="/logos/devlinks.svg"
-        alt="devlinks text"
-        priority
-        width="108"
-        height="21"
-      />
-    </div>
+      {isSmUp && (
+        <Image
+          src="/logos/devlinks.svg"
+          alt="devlinks text"
+          priority
+          width="108"
+          height="21"
+        />
+      )}
+    </Link>
   );
 }
