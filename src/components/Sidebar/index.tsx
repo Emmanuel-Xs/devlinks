dd031b1e3620538600da5d064d02e5385efbf65d
Emmanuel Xs <emmanuelxs101@gmail.com>
2024-09-19 18:37:50 +0100

    chore: move sidebar into a new folder

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PhoneFrame from "./PhoneFrame";
+
+export default function Sidebar() {
+  return (
+    <aside className="hidden h-[834px] w-full max-w-[560px] place-items-center rounded-xl bg-card p-6 min-[1200px]:grid">
+      <PhoneFrame />
+    </aside>
+  );
+}
