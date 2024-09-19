b255f075dc6c320891e971e73d9a55fd1f2ac401
Emmanuel Xs <emmanuelxs101@gmail.com>
2024-09-19 18:31:37 +0100

    style: styles changes to give the parents ownership of the  spacing between and around their children

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -7,9 +7,9 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <main className="grid min-h-svh grid-rows-[78px,_1fr] gap-4 pt-4">
+    <main className="mx-auto my-4 grid min-h-svh w-[min(100%_-_2rem,_1392px)] grid-rows-[78px,_1fr] gap-4 rounded-xl ring ring-red-500">
       <Navbar />
-      <section className="m-4 mx-auto mt-0 flex w-[min(100%_-_2rem,_1392px)] gap-4">
+      <section className="flex gap-4">
         <Sidebar />
         {children}
       </section>
