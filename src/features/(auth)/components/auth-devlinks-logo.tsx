f17148cd45594f9005e1b8bcbde300b05f3206a9
Emmanuel Xs <emmanuelxs101@gmail.com>
2024-11-20 14:56:01 +0100

    chore: remove priority attr as advice by console warning

--- src/features/(auth)/components/auth-devlinks-logo.tsx
+++ src/features/(auth)/components/auth-devlinks-logo.tsx
@@ -7,14 +7,12 @@ export default function DevlinksLogo({ className }: { className?: string }) {
       <Image
         src="/logos/devlinksLogo.svg"
         alt="icon logo of devlinks"
-        priority
         width="41"
         height="40"
       />
       <Image
         src="/logos/devlinks.svg"
         alt="devlinks text"
-        priority
         width="136"
         height="27"
       />
