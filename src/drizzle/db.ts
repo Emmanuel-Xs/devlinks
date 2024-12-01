228679c15822b7e48287dbfaeb4616f15b9adab9
Emmanuel Xs <emmanuelxs101@gmail.com>
2024-12-01 21:20:11 +0100

    fix: follows docs requirements

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,8 +1,11 @@
 // import { env } from "@/data/env/server";
-import { neon } from "@neondatabase/serverless";
+// import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 // import * as schema from "./schema";
 import { env } from "@/lib/server/serverEnv";
 
-const sql = neon(env.DATABASE_URL);
-export const db = drizzle(sql, { logger: true, casing: "snake_case" });
+// const sql = neon(env.DATABASE_URL);
+export const db = drizzle(env.DATABASE_URL, {
+  logger: true,
+  casing: "snake_case",
+});
