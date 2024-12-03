dde5361354796787452c7647e76edb580e0be41b
Emmanuel Xs <emmanuelxs101@gmail.com>
2024-12-03 23:10:27 +0100

    fix: use a tag to prevent nextjs Link prefetches

--- src/features/(auth)/components/oauth-buttons.tsx
+++ src/features/(auth)/components/oauth-buttons.tsx
@@ -6,11 +6,15 @@ import React from "react";
 export default function OAuthButtons() {
   return (
     <div className="grid grid-cols-2 gap-6">
-      <Button variant="outline" size="md">
-        <Mail className="mr-2 h-4 w-4" /> Google
+      <Button variant="outline" size="md" asChild>
+        <a href="/api/google">
+          <Mail className="mr-2 h-4 w-4" /> Google
+        </a>
       </Button>
-      <Button variant="outline" size="md">
-        <GitHubLogoIcon className="mr-2 h-4 w-4" /> GitHub
+      <Button variant="outline" size="md" asChild>
+        <a href="/api/github">
+          <GitHubLogoIcon className="mr-2 h-4 w-4" /> GitHub
+        </a>
       </Button>
     </div>
   );
