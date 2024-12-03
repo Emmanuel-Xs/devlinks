720c60f644390ed275d52f6a8a7527fe1e0a2faa
Emmanuel Xs <emmanuelxs101@gmail.com>
2024-12-03 23:56:55 +0100

    fix: makes oauth work both on dev and prod

--- src/lib/server/oauth.ts
+++ src/lib/server/oauth.ts
@@ -4,11 +4,15 @@ import { env } from "./serverEnv";
 export const github = new GitHub(
   env.GITHUB_CLIENT_ID,
   env.GITHUB_CLIENT_SECRET,
-  null,
+  process.env.NODE_ENV === "production"
+    ? `${env.HOME_URL}/api/github/callback`
+    : "http://localhost:3000/api/github/callback",
 );
 
 export const google = new Google(
   env.GOOGLE_CLIENT_ID,
   env.GOOGLE_CLIENT_SECRET,
-  "http://localhost:3000/api/google/callback",
+  process.env.NODE_ENV === "production"
+    ? `${env.HOME_URL}/api/google/callback`
+    : "http://localhost:3000/api/google/callback",
 );
