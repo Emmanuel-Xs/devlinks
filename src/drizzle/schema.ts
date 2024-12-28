c6606367244dcba9a43f8276d6b00a06c67fc455
Emmanuel Xs <emmanuelxs101@gmail.com>
2024-12-28 18:05:23 +0100

    feat: create links db schema

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,5 +1,13 @@
 import { InferSelectModel, relations } from "drizzle-orm";
-import { integer, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import {
+  index,
+  integer,
+  pgEnum,
+  pgTable,
+  text,
+  timestamp,
+  unique,
+} from "drizzle-orm/pg-core";
 
 const createdAt = timestamp({ withTimezone: true, mode: "date" })
   .defaultNow()
@@ -62,6 +70,44 @@ export const passwordResetSessions = pgTable("password_reset_sessions", {
   expiresAt,
 });
 
+export const platformEnum = pgEnum("platforms", [
+  "github",
+  "frontendmentor",
+  "twitter",
+  "linkedin",
+  "youtube",
+  "facebook",
+  "twitch",
+  "dev.to",
+  "codewars",
+  "codepen",
+  "freecodecamp",
+  "gitlab",
+  "hashnode",
+  "stackoverflow",
+]);
+
+export const links = pgTable(
+  "links",
+  {
+    id: integer().primaryKey().generatedByDefaultAsIdentity(),
+    userId: integer()
+      .notNull()
+      .references(() => usersTable.id, { onDelete: "cascade" }),
+    sequence: integer().notNull(),
+    url: text().notNull(),
+    platform: platformEnum().default("github"),
+    createdAt,
+    updatedAt,
+  },
+  (table) => [
+    unique().on(table.userId, table.sequence),
+    index("links_platform_idx").on(table.platform),
+  ],
+);
+
+// DB TYPES
+
 export type User = Pick<
   InferSelectModel<typeof usersTable>,
   keyof typeof returningUserData
@@ -82,6 +128,7 @@ export const usersRelations = relations(usersTable, ({ many }) => ({
   sessions: many(sessionsTable),
   emailVerificationRequests: many(emailVerificationRequest),
   passwordResetSessions: many(passwordResetSessions),
+  links: many(links),
 }));
 
 export const sessionsRelations = relations(sessionsTable, ({ one }) => ({
@@ -110,3 +157,10 @@ export const passwordResetSessionsRelations = relations(
     }),
   }),
 );
+
+export const linksRelations = relations(links, ({ one }) => ({
+  user: one(usersTable, {
+    fields: [links.userId],
+    references: [usersTable.id],
+  }),
+}));
