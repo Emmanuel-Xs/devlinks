184df8da0d38850ffe581499cca8d38fbc64aa51
Emmanuel Xs <emmanuelxs101@gmail.com>
2025-01-14 19:36:24 +0100

    fix: make id of link-input unique

--- src/features/(dashboard)/links/components/link-input.tsx
+++ src/features/(dashboard)/links/components/link-input.tsx
@@ -4,19 +4,23 @@ import { Input } from "../../../../components/ui/input";
 import { Label } from "../../../../components/ui/label";
 
 type LinkInputProps = {
+  id: number;
   url: string;
   // eslint-disable-next-line no-unused-vars
   onLinkChange: (url: string) => void;
 };
 
-export default function LinkInput({ url, onLinkChange }: LinkInputProps) {
+export default function LinkInput({ id, url, onLinkChange }: LinkInputProps) {
   return (
     <div className="space-y-1">
-      <Label htmlFor="link" className="leading-[150%] text-card-foreground">
+      <Label
+        htmlFor={`link-${id}`}
+        className="leading-[150%] text-card-foreground"
+      >
         Link
       </Label>
       <Input
-        id="link"
+        id={`link-${id}`}
         value={url}
         onChange={(e) => onLinkChange(e.target.value)}
         placeholder="m@example.com"
