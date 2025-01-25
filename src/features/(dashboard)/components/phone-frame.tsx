7d7574fadaa0ed232bfa7f1830bec11cce819372
Emmanuel Xs <emmanuelxs101@gmail.com>
2025-01-25 17:08:11 +0100

    feat (WIP): getting and styling user links

--- src/features/(dashboard)/components/phone-frame.tsx
+++ src/features/(dashboard)/components/phone-frame.tsx
@@ -1,18 +1,26 @@
 import Image from "next/image";
-import React from "react";
+import { ReactNode } from "react";
 
 import { cn } from "@/lib/utils";
 
-export default function PhoneFrame({ className }: { className?: string }) {
+export default function PhoneFrame({
+  className,
+  children,
+}: {
+  className?: string;
+  children: ReactNode;
+}) {
   return (
-    <Image
-      src="/images/phone-frame.svg"
-      alt="devlinks text"
-      priority
-      width="307"
-      height="631"
-      className={cn("", className)}
-      style={{ width: "auto" }}
-    />
+    <div className={cn("relative", className)}>
+      <Image
+        src="/images/phone-frame.svg"
+        alt="devlinks text"
+        priority
+        width="307"
+        height="631"
+        style={{ width: "auto" }}
+      />
+      {children}
+    </div>
   );
 }
--- src/features/(dashboard)/components/sidebar.tsx
+++ src/features/(dashboard)/components/sidebar.tsx
@@ -1,9 +1,14 @@
+import { ScrollArea } from "@/components/ui/scroll-area";
+
 import PhoneFrame from "./phone-frame";
 
 export default function Sidebar() {
   return (
-    <aside className="hidden h-full w-full max-w-[560px] place-items-center rounded-xl bg-card p-6 min-[1200px]:grid">
-      <PhoneFrame />
+    <aside className="hidden h-full w-full max-w-[560px] justify-center rounded-xl bg-card p-6 pt-14 min-[1200px]:grid">
+      <PhoneFrame className="relative">
+        <div></div>
+        <ScrollArea></ScrollArea>
+      </PhoneFrame>
     </aside>
   );
 }
