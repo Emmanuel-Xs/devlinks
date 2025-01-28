3e01a7282232f1db9c1c0bfa896c7855a6fc871b
Emmanuel Nwaohiri <emmanuelnwaohiri1@gmail.com>
2025-01-28 22:04:16 +0100

    fix: use Link with from next
    it errors if a tags is used as of next 15.1.6, used the Link tag with prefetch set to false

--- src/features/(auth)/components/oauth-buttons.tsx
+++ src/features/(auth)/components/oauth-buttons.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import Link from "next/link";
+
 import { GitHubLogoIcon } from "@radix-ui/react-icons";
 import { Mail } from "lucide-react";
 
@@ -9,14 +11,14 @@ export default function OAuthButtons() {
   return (
     <div className="grid grid-cols-2 gap-6">
       <Button variant="outline" size="md" asChild>
-        <a href="/api/google">
+        <Link href="/api/google" prefetch={false}>
           <Mail className="mr-2 h-4 w-4" /> Google
-        </a>
+        </Link>
       </Button>
       <Button variant="outline" size="md" asChild>
-        <a href="/api/github">
+        <Link href="/api/github" prefetch={false}>
           <GitHubLogoIcon className="mr-2 h-4 w-4" /> GitHub
-        </a>
+        </Link>
       </Button>
     </div>
   );
