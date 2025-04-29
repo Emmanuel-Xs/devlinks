07d6881f53126eef3d6e4f6004483a559c2660d7
Emmanuel Nwaohiri <emmanuelxs101@gmail.com>
2025-04-29 21:26:57 +0100

    Upgrade next and react and add animated icons for dashboard nav bar (#6)
    * feat: implement dialog for deletion and responsive design for mobile
    
    * chore: upgrade react to 19.1.0 and next to 15.3.1
    
    * feat: implement animated icons for dashboard navbar
    
    * fix: import react to pacify eslint

--- src/features/(dashboard)/components/navbar.tsx
+++ src/features/(dashboard)/components/navbar.tsx
@@ -1,6 +1,6 @@
-import { CircleUserRoundIcon, LinkIcon } from "lucide-react";
-
 import DevlinksLogo from "@/components/devlinks-logo";
+import { LinkIcon } from "@/components/ui/link-icon";
+import { UserIcon } from "@/components/ui/user-icon";
 
 import NavItem from "./nav-item";
 import PreviewLinkButton from "./preview-button";
@@ -11,14 +11,10 @@ export default function Navbar() {
       <menu className="flex items-center justify-between">
         <DevlinksLogo />
         <div className="flex items-center gap-4">
-          <NavItem
-            href="/links"
-            label="Links"
-            icon={<LinkIcon width={20} height={20} />}
-          />
+          <NavItem href="/links" label="Links" icon={<LinkIcon size={20} />} />
           <NavItem
             href="/profile"
-            icon={<CircleUserRoundIcon width={20} height={20} />}
+            icon={<UserIcon size={20} />}
             label="Profile Details"
           />
         </div>
