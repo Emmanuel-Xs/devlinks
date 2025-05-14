fede6daafa29502bbb5a05b1ad17ba33ad290009
Emmanuel Xs <emmanuelxs101@gmail.com>
2025-05-14 18:05:03 +0100

    feat: add animated icons

---
+++ src/components/ui/arrow-right-icon.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import type { Variants } from "motion/react";
+import { motion } from "motion/react";
+
+interface ArrowRightIconProps extends React.ComponentProps<typeof motion.svg> {
+  size?: number;
+}
+
+const pathVariants: Variants = {
+  normal: { d: "M5 12h14" },
+  animate: {
+    d: ["M5 12h14", "M5 12h9", "M5 12h14"],
+    transition: {
+      duration: 0.4,
+    },
+  },
+};
+
+const secondaryPathVariants: Variants = {
+  normal: { d: "m12 5 7 7-7 7", translateX: 0 },
+  animate: {
+    d: "m12 5 7 7-7 7",
+    translateX: [0, -3, 0],
+    transition: {
+      duration: 0.4,
+    },
+  },
+};
+
+export function ArrowRightIcon({ className, size = 28 }: ArrowRightIconProps) {
+  return (
+    <motion.svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="text-gray-400 transition-transform duration-300 group-hover:translate-x-0.5"
+    >
+      <motion.path
+        d="M5 12h14"
+        variants={pathVariants}
+        initial="normal"
+        whileHover="animate"
+      />
+      <motion.path
+        d="m12 5 7 7-7 7"
+        variants={secondaryPathVariants}
+        initial="normal"
+        whileHover="animate"
+      />
+    </motion.svg>
+  );
+}
---
+++ src/components/ui/logout-icon.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import type { Variants } from "motion/react";
+import { motion } from "motion/react";
+
+interface LogoutIconProps extends React.ComponentProps<typeof motion.svg> {
+  size?: number;
+}
+
+const pathVariants: Variants = {
+  animate: {
+    x: 2,
+    translateX: [0, -3, 0],
+    transition: {
+      duration: 0.4,
+    },
+  },
+};
+
+const LogoutIcon = ({ size = 28 }: LogoutIconProps) => {
+  return (
+    <motion.svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="group-hover:translate-x-0.5"
+    >
+      <motion.path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
+      <motion.polyline
+        points="16 17 21 12 16 7"
+        variants={pathVariants}
+        initial="normal"
+        whileHover="animate"
+      />
+      <motion.line
+        x1="21"
+        x2="9"
+        y1="12"
+        y2="12"
+        variants={pathVariants}
+        initial="normal"
+        whileHover="animate"
+      />
+    </motion.svg>
+  );
+};
+
+export { LogoutIcon };
