873f1d1bb22e8ecb95e0ba7e019b049c4cabc447
Emmanuel Xs <emmanuelxs101@gmail.com>
2025-12-18 10:46:12 +0100

    fix: update footer year from 2023 to current year

--- src/features/(home)/components/footer.tsx
+++ src/features/(home)/components/footer.tsx
@@ -5,7 +5,9 @@ export default function Footer() {
     <footer className="mt-12 border-t bg-background">
       <div className="mx-auto w-[min(100%_-_2.5rem,_1350px)] px-4 py-6">
         <div className="flex flex-col items-center justify-between gap-2 sm:flex-row">
-          <p>&copy; 2023 devlinks. All rights reserved.</p>
+          <p>
+            &copy; {new Date().getFullYear()} devlinks. All rights reserved.
+          </p>
           <nav>
             <menu className="flex space-x-4">
               <li>
